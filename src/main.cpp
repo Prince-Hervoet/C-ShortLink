566b0151d9fa5d3b9a8c35914956a6915d2a59e4
jammer <1378239869@qq.com>
2023-04-19 21:51:43 +0800

    feat: #add codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <string>
+#include <random>
 #include <chrono>
 
 static long long count = 100;
 static char base_chars[] = "efghdNDUVQRSTu01BCioEOP23Mpq567vwxyzA89arsjklmntWXYZ$4bcF!GHIJKL";
 static int flag = 0x3F;
+// 使用当前时间戳作为随机数种子
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+std::mt19937 gen(seed);
+// 创建均匀分布
+std::uniform_int_distribution<> dis(0, 63);
 
-static std::string toBase62(long long num)
+static std::string toShort(long long num, int minLength)
 {
     std::string result;
     int temp = 0;
@@ -15,6 +21,17 @@ static std::string toBase62(long long num)
         temp = (num & flag);
         num >>= 6;
         result += base_chars[temp];
+    }
+    if (result.size() < minLength)
+    {
+        int remain = minLength - result.size() - 1;
+        result += "_";
+        int index = 0;
+        for (int i = 0; i < remain; i++)
+        {
+            index = dis(gen);
+            result += base_chars[index];
+        }
     }
     return result;
 }
@@ -25,10 +42,8 @@ int main()
     auto now = std::chrono::system_clock::now();
     auto now_milliseconds = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
     auto start = now_milliseconds.time_since_epoch().count();
-    for (; count <= 80000000; ++count)
-    {
-        std::string ans = toBase62(count);
-    }
+    std::string ans = toShort(3, 6);
+    std::cout << ans << std::endl;
     // 获取当前时间戳
     now = std::chrono::system_clock::now();
     now_milliseconds = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
